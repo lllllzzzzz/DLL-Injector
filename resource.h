bcf383b11cbfd5cc6ad118e62a48621fed0ff9b3
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2015-06-25 14:07:22 +1000

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,16 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG1                             100
+#define IDC_SELECT_DLL                          40000
+#define IDC_CLOSE                               40001
+#define IDC_AUTO_REFRESH                        40002
+#define IDC_PROCESS_LIST                        40003
+#define IDC_ABOUT                               40004
+#define IDC_DLL_PATH                            40005
+#define IDC_REFRESH                             40006
+#define IDC_UNLOAD_DLL                          40007
+#define IDC_INJECT_DLL                          40008
+#define IDC_REMOTE_THREAD_INJECTION             40009
+#define IDC_INSTALL_FUNCTION_HOOK               40010
